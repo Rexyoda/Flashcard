9d8db949f12772559a3b8fd497bca7417f4e1592
Rexyoda <136438802+Rexyoda@users.noreply.github.com>
2023-10-21 23:14:05 +0900

    改善版

--- sample.js
+++ sample.js
@@ -1,29 +1,6 @@
 const flashcard = document.getElementById("flashcard");
-const front = document.querySelector(".front");
-const back = document.querySelector(".back");
+const flipButton = document.getElementById("flip-button");
 
-const flashcardData = [
-    { question: "質問1", answer: "答え1" },
-    { question: "質問2", answer: "答え2" },
-    // 他の質問と答えを追加
-];
-
-let currentCard = 0;
-
-function updateCard() {
-    front.textContent = flashcardData[currentCard].question;
-    back.textContent = flashcardData[currentCard].answer;
-}
-
-flashcard.addEventListener("click", function () {
-    flashcard.style.transform = "rotateY(180deg)";
+flipButton.addEventListener("click", function () {
+    flashcard.style.transform = flashcard.style.transform === "rotateY(180deg)" ? "rotateY(0deg)" : "rotateY(180deg)";
 });
-
-back.addEventListener("click", function (e) {
-    e.stopPropagation();
-    flashcard.style.transform = "rotateY(0deg)";
-    currentCard = (currentCard + 1) % flashcardData.length;
-    updateCard();
-});
-
-updateCard();
