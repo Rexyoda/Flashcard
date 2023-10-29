778826aa8ce161ec149c40c731085ca4333c8dee
sanmokugit <147925594+sanmokugit@users.noreply.github.com>
2023-10-29 22:30:39 +0900

    言葉変えた

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 'use strict';
 window.onload=()=>{
-	const animals=[
-		['ぞう','elephant'],
-		['しろくま','polarbear'],
-		['くじら','whale'],
-		['ペンギン','penguin'],
-		['ライオン','lion'],
-		['カンガルー','kangaroo'],
-		['ひと','human'],
-		['いぬ','dog'],
-		['ねこ','cat'],
-		['あり','ant'],
+	const words=[
+		['稼ぐ','earn'],
+		['運賃','fare'],
+		['正式な','formal'],
+		['合う','suit'],
+		['心配して','worried'],
+		['気付いて','aware'],
+		['残額','balance'],
+		['中身','content'],
+		['持ち上げる','lift'],
+		['行事','occasion'],
 	];
 
 	const ja =document.getElementById('ja');
@@ -23,14 +23,14 @@ window.onload=()=>{
 	btn.addEventListener('click',()=>{
 		let ans=entry.value.toLowerCase();
 		let msg='';
-		if (ans == animals[index][1]){
+		if (ans == words[index][1]){
 			correct++;
 			msg='正解!';
 		    }else{
-			msg=`不正解!${animals[index][0]}の英単語は${animals[index][1]}`;
+			msg=`不正解!${words[index][0]}の英単語は${words[index][1]}`;
 		}
 
-		if(index==animals.length-1){
+		if(index==words.length-1){
 			msg+=`<br>全${animals.length}問中,${correct}問正解`;
 			index=-1;
 		}
@@ -46,7 +46,7 @@ window.onload=()=>{
     
 	function setItem(index){
 		entry.value='';
-		ja.textContent=animals[index][0];
+		ja.textContent=words[index][0];
 	}
 	setItem(index);
 }
